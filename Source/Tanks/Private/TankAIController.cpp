975cb5bae6ce78946d7cb1a90539e773f6e7f0b3
Yael Maritz <yael.maritz@gmail.com>
2016-12-23 23:01:58 +0200

    Fixed the AI Class think
    Holy shit it worked

--- Source/Tanks/Private/TankAIController.cpp
+++ Source/Tanks/Private/TankAIController.cpp
@@ -2,12 +2,13 @@
 
 #include "Tanks.h"
 #include "Public/Tank.h"
-#include "Public/TankAIController.h"
+#include "TankAIController.h"
 
 // Begin Play
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
+
 	auto tank = Cast<ATank>(GetPawn());
 	if (!tank)
 	{
@@ -21,20 +22,19 @@ void ATankAIController::BeginPlay()
 // Tick
 void ATankAIController::Tick(float DeltaTime)
 {
-	Super::Tick( DeltaTime );
+	Super::Tick(DeltaTime);
 
-	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn(); // TODO: Find out why player pawn is not index 0
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
 	auto ThisTank = Cast<ATank>(GetPawn());
 
 	/// protect the pointer
 	if (PlayerPawn)
-	{ 
+	{
 		auto PlayerTank = Cast<ATank>(PlayerPawn);
 
 		// TODO: Move towards the player
 
 		/// Aim at player
-		// UE_LOG(LogTemp, Warning, TEXT("%s is aiming at %s"), *ThisTank->GetName(), *PlayerTank->GetName());
 		ThisTank->AimAt(PlayerTank->GetActorLocation());
 
 		/// Fire if ready
--- Source/Tanks/Public/TankAIController.h
+++ Source/Tanks/Public/TankAIController.h
@@ -2,19 +2,18 @@
 
 #pragma once
 
-#include "GameFramework/PlayerController.h"
+#include "AIController.h"
 #include "TankAIController.generated.h"
 
-// Forward decelerations
-class ATank;
-
 UCLASS()
-class TANKS_API ATankAIController : public APlayerController
+class TANKS_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+
 private:
 	virtual void BeginPlay() override;
-	
+
 	virtual void Tick(float DeltaSeconds) override;
+	
+	
 };
